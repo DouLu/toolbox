b6b887d640672efba8d0afa355cf59f50998ce88
DouLu <luckyludou@gmail.com>
2024-01-23 17:35:47 +0800

    feat: 拾起react和css的知识

--- src/pages/blog/admin/BlogList.tsx
+++ src/pages/blog/admin/BlogList.tsx
@@ -5,7 +5,7 @@ import {
   RadarChartOutlined,
   UserOutlined,
 } from "@ant-design/icons";
-import { Avatar, Button, Divider, Input, Space } from "antd";
+import { Avatar, Button, Divider, Input, Space, Spin } from "antd";
 import { useState } from "react";
 import styles from "./BlogList.module.css";
 
@@ -25,10 +25,52 @@ const menusData3 = [
   { text: "监控", icon: <PieChartOutlined /> },
   { text: "文件", icon: <PieChartOutlined /> },
 ];
+
+const listData1 = [
+  {
+    title: "第一个迭代",
+    desc: "try-basement/test-afc163 · 偏右 · 部署于12-14",
+    icon: <PieChartOutlined />,
+    extra: "test",
+  },
+  {
+    title: "第二个迭代",
+    desc: "try-basement/test-afc163 · 偏右 · 部署于12-14",
+    icon: <PieChartOutlined />,
+  },
+  {
+    title: "第三个迭代",
+    desc: "try-basement/test-afc163 · 偏右 · 部署于12-14",
+    icon: <PieChartOutlined />,
+  },
+];
 export default function BlogList() {
   const [isFold, setIsFold] = useState(false);
   const handleFold = (val: boolean) => {
     setIsFold(val);
+  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [listData, setListData] = useState(listData1);
+  const handleMore = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setListData([
+        ...listData,
+        ...[
+          {
+            title: "第4个迭代",
+            desc: "try-basement/test-afc163 · 偏右 · 部署于12-14",
+            icon: <PieChartOutlined />,
+          },
+          {
+            title: "第5个迭代",
+            desc: "try-basement/test-afc163 · 偏右 · 部署于12-14",
+            icon: <PieChartOutlined />,
+          },
+        ],
+      ]);
+      setIsLoading(false);
+    }, 3000);
   };
   const handleSearch = () => {};
   return (
@@ -41,10 +83,74 @@ export default function BlogList() {
       <div className={styles.rightContent}>
         <TopBar handleSearch={handleSearch} />
         <p className={styles.conTitle}>晚上好，今天又是忙碌的一天💪</p>
+        <Card
+          title="进行中的迭代"
+          content={listData.map((i) => (
+            <ListItem {...i} />
+          ))}
+          isLoading={isLoading}
+          handleMore={handleMore}
+        />
       </div>
     </div>
   );
 }
+
+const Card = ({
+  title,
+  extra,
+  content,
+  isLoading,
+  handleMore,
+}: {
+  title: string;
+  content: any;
+  extra?: any;
+  isLoading: boolean;
+  handleMore: () => void;
+}) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardTitle}>
+        <h5>{title}</h5>
+        {extra}
+      </div>
+      <div className={styles.cardContent}>{content}</div>
+      {isLoading ? (
+        <Spin />
+      ) : (
+        <Button block type="link" onClick={handleMore}>
+          查看更多
+        </Button>
+      )}
+    </div>
+  );
+};
+
+const ListItem = ({
+  title,
+  desc,
+  icon,
+  extra,
+}: {
+  title: string;
+  desc?: string;
+  icon?: any;
+  extra?: any;
+}) => {
+  return (
+    <div className={styles.listItem}>
+      <Space>
+        <div className={styles.itemIcon}>{icon}</div>
+        <div>
+          <h6>{title}</h6>
+          <p>{desc}</p>
+        </div>
+      </Space>
+      {extra}
+    </div>
+  );
+};
 
 const TopBar = ({ handleSearch }: { handleSearch: (val: string) => void }) => {
   return (
--- src/pages/blog/admin/Dashboard.tsx
+++ src/pages/blog/admin/Dashboard.tsx
@@ -1,3 +1,20 @@
+// 学习地址：https://blog.csdn.net/m0_57521762/article/details/131686432
+/**
+ * https://www.ruanyifeng.com/blog/2017/04/css_in_js.html
+ * 由于 CSS 的封装非常弱，导致了一系列的第三方库，用来加强 React 的 CSS 操作。它们统称为 CSS in JS，意思就是使用 JS 语言写 CSS。
+ * 根据不完全统计，各种 CSS in JS 的库至少有47种。老实说，现在也看不出来，哪一个库会变成主流。
+ */
+import styles from "./BlogList.module.css";
 export default function Dashboard() {
-  return <div>dashboard</div>;
+  const style = {
+    color: "red",
+    fontSize: "46px",
+  };
+  return (
+    <div>
+      <h3>react中三种样式隔离方案</h3>
+      <div className={styles.content}>css module</div>
+      <div style={style}>内联样式</div>
+    </div>
+  );
 }
