4bc6a6ea601cb8c9cd595d11ecb961b241a386c3
DouLu <luckyludou@gmail.com>
2024-05-14 17:07:56 +0800

    feat: 线上请求使用easy mock

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,7 +5,10 @@ const DATE_FORMATER = "YYYY/MM/DD";
 const FULL_DATE_FORMATER = "YYYY-MM-DD HH:mm:ss";
 const getCurrentTime = () => dayjs().format(FULL_DATE_FORMATER);
 
-const API_HOST = "http://127.0.0.1:8080/api/";
+const API_HOST =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8080/api/"
+    : "https://mock.mengxuegu.com/mock/6643266ce4a3c47b459391e4/toolbox/";
 
 const sleep = async (time = 1000) =>
   new Promise((resolve) => {
